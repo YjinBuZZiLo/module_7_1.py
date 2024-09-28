e758fcadbd2e40cc5db7e9499eec8d9ae19d2869
YjinBuZZiLo <evgene2305@gmail.com>
2024-09-28 23:59:43 +0300

    Add files via upload

--- 7_1.py
+++ 7_1.py
@@ -2,46 +2,31 @@ from pprint import pprint
 
 
 class Product:
-
     def __init__(self, name, weight, category):
         self.name = name
         self.weight = weight
         self.category = category
-
     def __str__(self):
-        return f'{self.name}, {self.weight}, {self.category}'
-
-
+        return (f'{self.name}, {self.weight}, {self.category}')
 class Shop:
-
     def __init__(self):
         self.__file_name = 'products.txt'
-
     def get_products(self):
         file = open(self.__file_name, 'r')
         products = file.read()
         file.close()
-        return str(products)
-
+        return products
     def add(self, *products):
-        file = open(self.__file_name, 'a')
-        new_products = self.get_products()
-        for product in products:
-            if product.name not in new_products:
-                file.write(str(product) + '\n')
-                new_products += product.name + '\n'
+        for i in products:
+            if i.name not in self.get_products():
+                file = open(self.__file_name, 'a')
+                file.write(i.__str__() + '\n')
             else:
-                print(f'Продукт {product.name} уже есть в магазине.')
-        file.close()
-
-
+                print(f'Продукт {i.name} уже есть в магазине')
 s1 = Shop()
 p1 = Product('Potato', 50.5, 'Vegetables')
 p2 = Product('Spaghetti', 3.4, 'Groceries')
 p3 = Product('Potato', 5.5, 'Vegetables')
-
-print(p2)  # __str__
-
+print(p2)
 s1.add(p1, p2, p3)
-
 print(s1.get_products())
